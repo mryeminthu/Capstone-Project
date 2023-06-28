f96f28d0d677f11bdaccbeb7d4cf4a380c66f12b
mryeminthu <dhammaarchive@hotmail.com>
2023-06-28 10:53:26 -0700

    Fix-linters

--- app.js
+++ app.js
@@ -1,70 +1,70 @@
 // Hamburger
-const toggleBtn = document.getElementsByClassName("toggle")[0];
-const fiveMenu = document.getElementsByClassName("five-menu")[0];
-const sect1 = document.getElementsByTagName("section")[0];
-const sect2 = document.getElementsByTagName("section")[1];
-const sect3 = document.getElementsByTagName("section")[2];
-const sect4 = document.getElementsByTagName("section")[3];
-const footer = document.getElementsByTagName("footer")[0];
-toggleBtn.addEventListener("click", () => {
-  fiveMenu.classList.toggle("active");
-  sect1.style.display = "none";
-  sect2.style.display = "none";
-  sect3.style.display = "none";
-  sect4.style.display = "none";
-  footer.style.display = "none";
+const toggleBtn = document.getElementsByClassName('toggle')[0];
+const fiveMenu = document.getElementsByClassName('five-menu')[0];
+const sect1 = document.getElementsByTagName('section')[0];
+const sect2 = document.getElementsByTagName('section')[1];
+const sect3 = document.getElementsByTagName('section')[2];
+const sect4 = document.getElementsByTagName('section')[3];
+const footer = document.getElementsByTagName('footer')[0];
+toggleBtn.addEventListener('click', () => {
+  fiveMenu.classList.toggle('active');
+  sect1.style.display = 'none';
+  sect2.style.display = 'none';
+  sect3.style.display = 'none';
+  sect4.style.display = 'none';
+  footer.style.display = 'none';
 });
-const crossBtn = document.getElementsByClassName("cross")[0];
-crossBtn.addEventListener("click", () => {
-  fiveMenu.classList.remove("active");
-  sect1.style.display = "block";
-  sect2.style.display = "block";
-  sect3.style.display = "block";
-  sect4.style.display = "block";
-  footer.style.display = "block";
+const crossBtn = document.getElementsByClassName('cross')[0];
+crossBtn.addEventListener('click', () => {
+  fiveMenu.classList.remove('active');
+  sect1.style.display = 'block';
+  sect2.style.display = 'block';
+  sect3.style.display = 'block';
+  sect4.style.display = 'block';
+  footer.style.display = 'block';
 });
-fiveMenu.addEventListener("click", () => {
-  fiveMenu.classList.remove("active");
+fiveMenu.addEventListener('click', () => {
+  fiveMenu.classList.remove('active');
 });
 
-//Coaches
+// Coaches
 const coaches = [
   {
     name: 'Aung La N Sang',
     description: 'Born in Myanmar, he currently competes in both the middleweight and light heavyweight divisions.',
     imageSrc: 'images/aungla.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Anatoly Malykhin',
     description: 'The current ONE Light Heavyweight Champion and ONE Heavyweight World Champion',
     imageSrc: 'images/malykhin.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Arjan Bhullar',
     description: 'The former ONE Heavyweight World Champion',
     imageSrc: 'images/bhullar.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Rodtang Jitmuangnon',
     description: 'He currently competes in ONE Championship and is the reigning ONE Flyweight Muay Thai World Champion.',
     imageSrc: 'images/rodtang.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Christian Lee',
     description: 'Lee is the fourth two-division champion in ONE history and holds the promotion\'s records for most wins (17), most finishes (16), and most knockouts (12).',
     imageSrc: 'images/christian.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
   },
   {
     name: 'Vitaly Bigdash',
     description: 'Vitaly Bigdash was a former ONE Middleweight World Champion.',
     imageSrc: 'images/vitaly.jpg',
-    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-  }
+    bio: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
 ];
 
 const coachesContainer = document.querySelector('.six-box');
